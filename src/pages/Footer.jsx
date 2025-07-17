a431666f6dd8001c549fc11e59f5e268699c123b
adwattar <wattarxxxa@gmail.com>
2025-07-17 03:39:21 +0300

    Fix: add vite as devDependency

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -16,17 +16,14 @@ export default function Footer() {
       <p className="text-xs mt-1 text-gray-500">© {new Date().getFullYear()} – جميع الحقوق محفوظة</p>
 
       <div className="flex justify-center gap-6 mt-4">
-        <a href="https://github.com/ahmed" target="_blank" className="hover:text-white transition">
+        <a href="https://github.com/adwattar" target="_blank" className="hover:text-white transition">
           <Github size={20} />
         </a>
-        <a href="https://linkedin.com/in/ahmed" target="_blank" className="hover:text-white transition">
+        <a href="https://www.linkedin.com/in/ahmed-motamed-4315b02a8" target="_blank" className="hover:text-white transition">
           <Linkedin size={20} />
         </a>
-        <a href="mailto:ahmed@example.com" className="hover:text-white transition">
+        <a href="mailto:wattarxxxa@example.com" className="hover:text-white transition">
           <Mail size={20} />
-        </a>
-        <a href="https://instagram.com/ahmed" target="_blank" className="hover:text-white transition">
-          <Instagram size={20} />
         </a>
       </div>
     </footer>
